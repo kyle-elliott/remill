c810e20e02a4493ce39d84d3596e7f1cbebfa2db
Kyle Elliott <kyle_a_elliott@protonmail.ch>
2024-11-13 11:08:26 -0500

    Wrap BC generation in flag
    Helps windows users compile and link on windows, however they will still need to pull the generated BC files from an external source to use the library properly

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -24,6 +24,7 @@ set(REMILL_INSTALL_LIB_DIR "${CMAKE_INSTALL_LIBDIR}" CACHE PATH "Directory in wh
 set(REMILL_INSTALL_BIN_DIR "${CMAKE_INSTALL_BINDIR}" CACHE PATH "Directory in which remill binaries will be installed")
 set(REMILL_INSTALL_INCLUDE_DIR "${CMAKE_INSTALL_INCLUDEDIR}" CACHE PATH "Directory in which remill headers will be installed")
 set(REMILL_INSTALL_SHARE_DIR "${CMAKE_INSTALL_DATADIR}" CACHE PATH "Directory in which remill cmake files will be installed")
+option(REMILL_ENABLE_BC_GENERATION "Should Remill generate bitcode?" TRUE)
 option(REMILL_ENABLE_INSTALL_TARGET "Should Remill be installed?" TRUE)
 cmake_dependent_option(REMILL_ENABLE_TESTING "Build your tests" ON "can_enable_testing" OFF)
 cmake_dependent_option(REMILL_ENABLE_TESTING_X86 "Build your tests" ON "REMILL_ENABLE_TESTING;can_enable_testing_x86" OFF)
